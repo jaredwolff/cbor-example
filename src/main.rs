bd8e427a25b33fb27ebee93e0cdd9a1d3f7e2a31
Jared Wolff <hello@jaredwolff.com>
2020-11-18 17:51:54 -0500

    First commit of Rust example code for CBOR

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+use serde_cbor;
+use warp::{hyper::body, Filter};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct TelemetryData {
+    version: String,
+    rssi: i32,
+}
+
+#[derive(Deserialize, Serialize)]
+struct TelemetryError {
+    error: String,
+}
+
+#[tokio::main]
+async fn main() {
+    // POST /telemetry/:id  {"version":"0.1.0","rssi":-49}
+    let telemetry = warp::post()
+        .and(warp::path("telemetry"))
+        .and(warp::path::param::<String>())
+        .and(warp::body::content_length_limit(1024))
+        .and(warp::body::bytes())
+        .map(|id, data: body::Bytes| {
+            println!("Message from id: {}", id);
+
+            // Get the telemetry
+            let telemetry: TelemetryData = match serde_cbor::from_slice(&data) {
+                Ok(t) => t,
+                Err(_) => {
+                    // Create error
+                    let error = TelemetryError {
+                        error: "Unable to parse telemetry data.".to_string(),
+                    };
+
+                    // Return error
+                    return warp::reply::json(&error);
+                }
+            };
+
+            println!("Telemetry: {:?}", telemetry);
+
+            warp::reply::json(&{})
+        });
+
+    warp::serve(telemetry).run(([127, 0, 0, 1], 3030)).await
+}
